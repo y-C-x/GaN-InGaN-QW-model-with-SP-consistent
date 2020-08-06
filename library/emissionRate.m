eaa034416d9ded7dd5f3bbf91b0d0b3cf30e89d0
y-C-x <47716850+y-C-x@users.noreply.github.com>
2020-08-06 14:41:11 -0400

    Add files via upload

--- library/emissionRate.m
+++ library/emissionRate.m
@@ -1,7 +1,7 @@
 classdef emissionRate
     properties
         C0;
-        C1;
+        C1; % [1/J-m^2-s]
         oL; % over lap
         mmE; % momentum matrix element
         rho_CH1; % density of state for CH1-HH1 transition
@@ -56,6 +56,13 @@ classdef emissionRate
         r_sp_CL2; % emission rate
         
         r_sp; % [1/eV-s-m^3]
+        
+        r_sp_CH1_broad; % emission rate
+        r_sp_CL1_broad; % emission rate
+        r_sp_CH2_broad; % emission rate
+        r_sp_CL2_broad; % emission rate
+        
+        r_sp_broad; % [1/eV-s-m^3]
     end
     
     
@@ -65,14 +72,14 @@ classdef emissionRate
             rate.oL = overLap;
             rate.mmE = MMElement;
             rate.wavLen = waveLength * 1e-9;
-            rate.hw = flip(h*c0./rate.wavLen /q); % unit of Joule to eV
+            rate.hw = (h*c0./rate.wavLen /q); % unit of Joule to eV
             rate.w = c0./rate.wavLen;
         end
         
         function rate = updateC(rate)
             global h_b q c0 eps0 m0
             rate.C0 = pi*q^2/rate.nr/c0/eps0/m0^2.*(rate.w).^-1; % SI Unit [m^2/kg]
-            rate.C1 = rate.nr^2.*rate.w.^2/pi^2/h_b/c0^2; % SI unit
+            rate.C1 = rate.nr^2.*rate.w.^2/pi^2/h_b/c0^2; % SI unit [1/J-m^2-s]
         end
         
         function rate = getRho(rate,Fermi,ES)
@@ -128,10 +135,10 @@ classdef emissionRate
         
         function rate = getSponRate(rate)
             global q;
-            rate.r_sp_CH1 = rate.oL.Ic1_hh1*rate.C0.*rate.C1.*1.5*rate.mmE.C1_HH1.*rate.rho_CH1.*rate.fc_c1_hh1.*(1-rate.fv_c1_hh1)*q;
-            rate.r_sp_CL1 = rate.oL.Ic1_lh1*rate.C0.*rate.C1.*0.5*rate.mmE.C1_LH1.*rate.rho_CL1.*rate.fc_c1_lh1.*(1-rate.fv_c1_lh1)*q;
-            rate.r_sp_CH2 = rate.oL.Ic2_hh2*rate.C0.*rate.C1.*1.5*rate.mmE.C2_HH2.*rate.rho_CH2.*rate.fc_c2_hh2.*(1-rate.fv_c2_hh2)*q;
-            rate.r_sp_CL2 = rate.oL.Ic2_lh2*rate.C0.*rate.C1.*0.5*rate.mmE.C2_LH2.*rate.rho_CL2.*rate.fc_c2_lh2.*(1-rate.fv_c2_lh2)*q;
+            rate.r_sp_CH1 = rate.oL.Ic1_hh1*rate.C0.*rate.C1.*1.5*rate.mmE.C1_HH1.*rate.rho_CH1.*rate.fc_c1_hh1.*(1-rate.fv_c1_hh1)*q*q; % [1/eV-s-m^3]
+            rate.r_sp_CL1 = rate.oL.Ic1_lh1*rate.C0.*rate.C1.*0.5*rate.mmE.C1_LH1.*rate.rho_CL1.*rate.fc_c1_lh1.*(1-rate.fv_c1_lh1)*q*q;
+            rate.r_sp_CH2 = rate.oL.Ic2_hh2*rate.C0.*rate.C1.*1.5*rate.mmE.C2_HH2.*rate.rho_CH2.*rate.fc_c2_hh2.*(1-rate.fv_c2_hh2)*q*q;
+            rate.r_sp_CL2 = rate.oL.Ic2_lh2*rate.C0.*rate.C1.*0.5*rate.mmE.C2_LH2.*rate.rho_CL2.*rate.fc_c2_lh2.*(1-rate.fv_c2_lh2)*q*q;
             rate.r_sp = rate.r_sp_CH1+rate.r_sp_CH2+rate.r_sp_CL1+rate.r_sp_CL2;
         end
         
@@ -175,6 +182,38 @@ classdef emissionRate
             rate.g_linewidth_CL2_broad = rate.C0*rate.oL.Ic2_lh2*0.5*rate.mmE.C2_LH2.*rate.rhoC_ch2_lh2.*(rate.fc_c2_lh2-rate.fv_c2_lh2).*f_c2_lh2*q; %[1/m]
             rate.g_linewidth_broad = rate.g_linewidth_CH1_broad + rate.g_linewidth_CL1_broad + rate.g_linewidth_CH2_broad + rate.g_linewidth_CL2_broad;
 
+        end
+        
+        
+        function rate = getSponRate_broad(rate,gamma,ES)
+            global q kT
+            
+            Et_c1_hh1 = rate.hw - ES.Ec1_hh1;
+            Et_c1_lh1 = rate.hw - ES.Ec1_lh1;
+            Et_c2_hh2 = rate.hw - ES.Ec2_hh2;
+            Et_c2_lh2 = rate.hw - ES.Ec2_lh2;
+            
+            factor = @(Et,Etx) (gamma/pi)./((-Et+Etx).^2+gamma^2);
+            Etx = 0 : 0.01 : 5;
+            
+            f_c1_hh1 = zeros(1,length(Et_c1_hh1));
+            f_c1_lh1 = zeros(1,length(Et_c1_lh1));
+            f_c2_hh2 = zeros(1,length(Et_c2_hh2));
+            f_c2_lh2 = zeros(1,length(Et_c2_lh2));
+            
+            for i = 1 : length(Et_c1_hh1)
+                f_c1_hh1(i) = sum(factor(Et_c1_hh1(i),Etx))/100;
+                f_c1_lh1(i) = sum(factor(Et_c1_lh1(i),Etx))/100;
+                f_c2_hh2(i) = sum(factor(Et_c2_hh2(i),Etx))/100;
+                f_c2_lh2(i) = sum(factor(Et_c2_lh2(i),Etx))/100;
+            end
+            
+            rate.r_sp_CH1_broad = rate.oL.Ic1_hh1*rate.C0.*rate.C1.*1.5*rate.mmE.C1_HH1.*rate.rhoC_ch1_hh1.*rate.fc_c1_hh1.*(1-rate.fv_c1_hh1).*f_c1_hh1*q*q; % [1/eV-s-m^3]
+            rate.r_sp_CL1_broad = rate.oL.Ic1_lh1*rate.C0.*rate.C1.*0.5*rate.mmE.C1_LH1.*rate.rhoC_ch1_lh1.*rate.fc_c1_lh1.*(1-rate.fv_c1_lh1).*f_c1_lh1*q*q;
+            rate.r_sp_CH2_broad = rate.oL.Ic2_hh2*rate.C0.*rate.C1.*1.5*rate.mmE.C2_HH2.*rate.rhoC_ch2_hh2.*rate.fc_c2_hh2.*(1-rate.fv_c2_hh2).*f_c2_hh2*q*q;
+            rate.r_sp_CL2_broad = rate.oL.Ic2_lh2*rate.C0.*rate.C1.*0.5*rate.mmE.C2_LH2.*rate.rhoC_ch2_lh2.*rate.fc_c2_lh2.*(1-rate.fv_c2_lh2).*f_c2_lh2*q*q;
+            
+            rate.r_sp_broad = rate.r_sp_CH1_broad + rate.r_sp_CL1_broad + rate.r_sp_CH2_broad + rate.r_sp_CL2_broad;
         end
         
         
